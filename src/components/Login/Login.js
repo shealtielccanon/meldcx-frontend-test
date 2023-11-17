097eecee79a3fd206fab38b1cf82aade29278308
Shealtiel Canon <shealtielccanon@gmail.com>
2023-11-17 15:21:21 +0800

    update password field

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -70,6 +70,7 @@ const Login = (props) => {
         <br />
         <div className={"inputContainer"}>
             <input
+                type={"password"}
                 value={password}
                 placeholder="Enter your password here"
                 onChange={ev => setPassword(ev.target.value)}
